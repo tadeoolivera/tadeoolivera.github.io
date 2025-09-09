7e90c795f8c283955f3fc397416605af44045b36
Tadeo Olivera <tadeooliverabloops@gmail.com>
2025-09-09 00:13:40 -0300

    Delete private directory

--- private/main/src/controllers/accessController.js
+++
@@ -1,6 +0,0 @@
-const token = sessionStorage.getItem('accessToken');
-
-// Si no hay token válido en sessionStorage, redirige al inicio
-if (!token) {
-  window.location.href = '/index.html?access=private';
-}
